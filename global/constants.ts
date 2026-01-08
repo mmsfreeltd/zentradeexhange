1127b86c455c0c450114aae30612a9ffaf16fd59
dogunfx <dogunfx@gmail.com>
2026-01-08 12:12:16 +0100

    updated site info

--- global/constants.ts
+++ global/constants.ts
@@ -4,4 +4,13 @@ export const PROFILE_IMAGE_PLACE_HOLDER: string = '/images/profile.jpg';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
 export const LIVE_CHAT = `
 
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'd31499140457af5bf073dd6eb069b8f7140ccca1';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+
 `;
